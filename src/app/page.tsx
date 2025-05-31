8ff0bc74611f00004d7452761463f5539dbfc40a
Raman Kumar <rku.430@gmail.com>
2025-05-31 20:07:22 +0530

    05 authentication socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,15 @@
-"use client";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { authClient } from "@/lib/auth-client";
-import { Button } from "@/components/ui/button";
+export default async function Home() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
 
-export default function Home() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const { data: session } = authClient.useSession();
-
-	const onSubmit = () => {
-		authClient.signUp.email(
-			{
-				email,
-				name,
-				password,
-			},
-			{
-				onError: () => alert("Something Went Wrong"),
-				onSuccess: () => alert("Success"),
-			}
-		);
-	};
-
-  if(session) {
-    return (
-      <div className="flex flex-col gap-y-4">
-        <p>Logged in as {session.user.name}</p>
-        <Button onClick={() => authClient.signOut()}>
-          Log Out
-        </Button>
-      </div>
-    )
-  }
-	return (
-		<div className="flex flex-col gap-y-4 p-4">
-			<Input
-				placeholder="Enter Name"
-				value={name}
-				onChange={(e) => setName(e.target.value)}
-			/>
-			<Input
-				placeholder="Enter email"
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<Input
-				placeholder="Enter password"
-				type="password"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<Button onClick={onSubmit}>Create User</Button>
-		</div>
-	);
+	if (!session) {
+		redirect("/sign-in");
+	}
+	return <HomeView />;
 }
