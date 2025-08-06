17dc93e10054dd837a497c4d4601a13a9b374506
Raman Kumar <rku.430@gmail.com>
2025-08-06 22:25:43 +0530

    11: Agent Form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {TRPCReactProvider} from "@/trpc/client";
+import { TRPCReactProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -13,18 +14,19 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <TRPCReactProvider>
             <html lang="en" className="hydrated">
-            <body
-                className={`${inter.className} antialiased`}
-            >
-            {children}
-            </body>
+                <body
+                    className={`${inter.className} antialiased`}
+                >
+                    <Toaster />
+                    {children}
+                </body>
             </html>
         </TRPCReactProvider>
     );
---
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -0,0 +1,120 @@
+import { useTRPC } from "@/trpc/client";
+import { AgentGetOne } from "../../types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { agentsInsertSchema } from "../../schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "sonner";
+
+interface AgentFormProps {
+  onSuccess?: () => void;
+  onCancel?: () => void;
+  initialValues?: AgentGetOne;
+}
+
+export const AgentForm = ({
+  onSuccess,
+  onCancel,
+  initialValues
+}: AgentFormProps) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const createAgent = useMutation(
+    trpc.agents.create.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions())
+
+        if (initialValues?.id) {
+          await queryClient.invalidateQueries(trpc.agents.getOne.queryOptions({ id: initialValues.id }))
+        }
+
+        onSuccess?.();
+      },
+      onError: (error) => {
+        toast.error(error.message)
+      }
+    })
+  )
+
+  const form = useForm<z.infer<typeof agentsInsertSchema>>({
+    resolver: zodResolver(agentsInsertSchema),
+    defaultValues: {
+      name: initialValues?.name ?? "",
+      instruction: initialValues?.instruction ?? ""
+    }
+  })
+
+  const isEdit = !!initialValues?.id;
+  const isPending = createAgent.isPending;
+
+  const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
+    if (isEdit) {
+      console.log("TODO: update agent");
+    } else {
+      createAgent.mutate(values)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <GeneratedAvatar
+          seed={form.watch("name")}
+          variant="botttsNeutral"
+          classname="border size-16"
+        />
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="e.g. math tutor" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="instruction"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Instructions</FormLabel>
+              <FormControl>
+                <Textarea {...field} placeholder="e.g. you a helpful maths tutor that can help in solving math assignments." />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-between items-center">
+          {onCancel && (
+            <Button
+              type="button"
+              variant={"ghost"}
+              disabled={isPending}
+              onClick={onCancel}
+            >
+              Cancel
+            </Button>
+          )}
+          <Button
+            disabled={isPending}
+            type="submit"
+          >
+            {isEdit ? "Update" : "Create"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
