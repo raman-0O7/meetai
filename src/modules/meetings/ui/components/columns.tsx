54bd4afbe76e1ade0b039b340ffc4ac547b7dfce
Raman Kumar <rku.430@gmail.com>
2025-09-20 21:33:25 +0530

    25: completed state

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -1,21 +1,12 @@
 "use client";
 
 import { format } from "date-fns";
-import humanizeDuration from "humanize-duration";
 import { ColumnDef } from "@tanstack/react-table"
 import { MeetingGetMany } from "../../types";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { CircleCheckIcon, CircleXIcon, ClockArrowUpIcon, ClockFadingIcon, CornerDownRightIcon, LoaderIcon } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
-import { cn } from "@/lib/utils";
-
-function formatDuration(seconds: number) {
-  return humanizeDuration(seconds * 1000, {
-    round: true,
-    largest: 1,
-    units: ["h", "m", "s"]
-  })
-}
+import { cn, formatDuration } from "@/lib/utils";
 
 const statusIconMap = {
   upcoming: ClockArrowUpIcon,
